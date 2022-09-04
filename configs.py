de2bc19e429441402ceadd6ffdd06ab18a14f208
hackerheroOP <78025240+hackerheroOP@users.noreply.github.com>
2022-09-04 15:35:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,20 +4,20 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = os.environ.get("API_ID", "1560761")
+    API_HASH = os.environ.get("API_HASH", "d7e3b89b16213382fa173a9c3b5d6cc4")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "good-man")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
     LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
     DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
-    TIME_GAP = int(os.environ.get("TIME_GAP", 5))
-    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
-    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
-    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-    MONGODB_URI = os.environ.get("MONGODB_URI")
+    TIME_GAP = int(os.environ.get("TIME_GAP", 1))
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 10))
+    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "465b538caae524fecfea")
+    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "bQkVD0V0RzuP0l3")
+    MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://papa:papa@cluster0.pyg1s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1251111009)
 
     START_TEXT = """
 Hi Unkil, I am Video Merge Bot!
